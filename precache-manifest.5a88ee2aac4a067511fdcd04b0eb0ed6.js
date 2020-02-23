2d54dfe326f3e1254464e1f2df0d5dfe73990a77
Clara Lecroisey <clalecroisey@gmail.com>
2020-02-23 10:51:33 +0100

    Updates

---
+++ precache-manifest.5a88ee2aac4a067511fdcd04b0eb0ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6777fb2643634a2e9b04ce194eb7ee2d",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "765ccfb9f458b32b33f3",
+    "url": "/blindtest-spotify/static/css/main.5ec250b0.chunk.css"
+  },
+  {
+    "revision": "dff975b98fce27007731",
+    "url": "/blindtest-spotify/static/js/2.88fcf1aa.chunk.js"
+  },
+  {
+    "revision": "765ccfb9f458b32b33f3",
+    "url": "/blindtest-spotify/static/js/main.8722aa9d.chunk.js"
+  },
+  {
+    "revision": "d74675ac0a498334cc92",
+    "url": "/blindtest-spotify/static/js/runtime-main.07f395c8.js"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.5a88ee2aac4a067511fdcd04b0eb0ed6.js"
 );
 
 self.addEventListener('message', (event) => {
