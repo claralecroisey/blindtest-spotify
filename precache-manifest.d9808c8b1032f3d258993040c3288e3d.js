9e90907e109ac7b99dfd1ed26078d80ee43466c9
Clara Lecroisey <clalecroisey@gmail.com>
2020-02-26 00:59:16 +0100

    Updates

---
+++ precache-manifest.d9808c8b1032f3d258993040c3288e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62437a1fa2ba5426217c16c3f0a95d4f",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "1087fa28007fd63e4219",
+    "url": "/blindtest-spotify/static/css/main.c5ae755f.chunk.css"
+  },
+  {
+    "revision": "d8848af21af58fef1c6d",
+    "url": "/blindtest-spotify/static/js/2.fe634c22.chunk.js"
+  },
+  {
+    "revision": "1087fa28007fd63e4219",
+    "url": "/blindtest-spotify/static/js/main.a21bf6a6.chunk.js"
+  },
+  {
+    "revision": "d74675ac0a498334cc92",
+    "url": "/blindtest-spotify/static/js/runtime-main.07f395c8.js"
+  },
+  {
+    "revision": "e359ad39480a3ed8d413d0a663bce8a7",
+    "url": "/blindtest-spotify/static/media/logo.e359ad39.svg"
+  }
+]);
